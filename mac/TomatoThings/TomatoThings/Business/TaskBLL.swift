ba8a0d093399204d92ba7d1064e3b1474c692b8c
iHugo <cythbgy@gmail.com>
2016-02-14 09:25:29 +0800

    readme

--- mac/TomatoThings/TomatoThings/Business/TaskBLL.swift
+++ mac/TomatoThings/TomatoThings/Business/TaskBLL.swift
@@ -49,7 +49,7 @@ class TaskBLL: NSObject {
   
   private func startTimer() {
     startDate = NSDate()
-    timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "onTimerFire:", userInfo: nil, repeats: true)
+    timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "onTimerFire:", userInfo: nil, repeats: true)
   }
   private func invalidateTimer() {
     timer.invalidate()
